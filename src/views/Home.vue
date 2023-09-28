d4779f050abfe9000df37870834d510f62e5b5dc
Simphiwe Tebe <simphiwetebe@gmail.com>
2023-09-28 13:05:41 +0200

    starting dynamic routes and home page layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,7 +17,7 @@
   </div>
 
   <div class="flex justify-center gap-2 mt-2">
-    <router-link to="/" v-for="letter of letters">
+    <router-link :to="{ name: 'byLetter', params: {letter}}" v-for="letter of letters" :key="letter">
       {{ letter }}
     </router-link>
   </div>
