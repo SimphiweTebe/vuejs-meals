5a4c219074f300ed9459bd260f099b75c3b5c73c
Simphiwe Tebe <simphiwetebe@gmail.com>
2023-10-05 19:04:56 +0200

    Created meal and mealCollection components. Created actions for fetching byName and byLetter

--- src/store/actions.js
+++ src/store/actions.js
@@ -4,3 +4,13 @@ export const searchMeals = async ({ commit }, keyword) =>{
   const {data} = await axiosClient.get(`search.php?s=${keyword}`)
   commit('setSearchedMeals', data.meals)
 }
+
+export const searchMealByLetter = async ({ commit }, letter) =>{
+  const {data} = await axiosClient.get(`search.php?f=${letter}`)
+  commit('setMealsByLetter', data.meals)
+}
+
+export const searchMealByIngredient = async ({ commit }, ingredient) =>{
+  const {data} = await axiosClient.get(`filter.php?i=${ingredient}`)
+  commit('setMealsByIngredient', data.meals)
+}
